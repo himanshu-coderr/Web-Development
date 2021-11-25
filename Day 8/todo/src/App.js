3225b490ebd11123ad2379930aa0ba6a9bce2e83
Himanshu Mehta <hmehtaengg@gmail.com>
2021-11-25 19:49:46 +0530

    Todo list

---
+++ Day 8/todo/src/App.js
@@ -0,0 +1,17 @@
+import React, { useReducer } from 'react'
+import Todo from './component/todoreact/todo'
+
+const App = () => {
+  return (
+    <div>
+      <Todo/>
+    </div>
+  )
+}
+
+const MyName = () =>{
+  return (
+    <h2>Himanshu</h2>
+  )
+}
+export default App
